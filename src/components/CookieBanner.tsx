95f943c7810b3e27940cb28160ab1db262dbacc2
Guy Brown <guy.brown@justchecking.co.uk>
2023-11-01 11:49:24 +0000

    cookie banner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -33,10 +33,10 @@ export default function CookieBanner(){
 				cookieConsent != null ? "hidden" : "block",
 				"pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6 z-50"
 			)}>
-				<div className="pointer-events-auto mx-auto max-w-xl rounded-xl bg-white p-6 shadow-lg ring-1 ring-gray-900/10">
-					<p className="text-sm leading-6 text-gray-900">
+				<div className="pointer-events-auto mx-auto max-w-xl rounded-xl bg-white p-6 shadow-lg ring-1 ring-slate-900/10">
+					<p className="text-base leading-6 text-slate-900">
 						We use cookies to enhance your browsing experience and analyse our traffic. By clicking <span className='font-semibold'>Accept All</span>, you consent to our use of cookies. See our{' '}
-						<Link href="/policies/cookie-policy" className="font-semibold text-green-600">cookie policy</Link>
+						<Link href="/policies/cookie-policy" className="font-semibold text-green-600">cookie policy</Link> for more information on cookies.
 						.
 					</p>
 					<div className="mt-4 flex items-center gap-x-5">
